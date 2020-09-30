f02cce11488b4966a9cc98a4959dc9a9a7a00d88
Chandana3 <30951763+Chandana3@users.noreply.github.com>
2020-09-30 17:05:47 +0530

    added few more os function

--- os_node.js
+++ os_node.js
@@ -4,7 +4,11 @@ let platform=os.platform();
 let arc=os.arch();
 let hostName=os.hostname();
 let freeMem=os.freemem();
-let homeDir=os.homedir()
+let homeDir=os.homedir();
+let loadavg=os.loadavg();
+let type=os.type();
+let version=os.version();
+let dir=os.tmpdir();
 let networkInterfaces=os.networkInterfaces();
-console.log(`${release} ${platform} ${arc} ${os.hostname()}`);
+console.log(`${release} ${platform} ${arc}  ${type} ${version} ${dir}`);
 
