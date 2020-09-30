31939fa04e68de077a48aa4933855ef3244a8146
Chandana3 <30951763+Chandana3@users.noreply.github.com>
2020-09-30 13:01:05 +0530

    multiply 2 numbers

---
+++ product.js
@@ -0,0 +1,6 @@
+function product(firstNumber,secondNumber)
+{
+    let result=firstNumber*secondNumber;
+    console.log("Result of multiplication IS: "+result);
+}
+module.exports.multiply=product;
