2def73c047f38321bb5b726749ed5508ab0a8d87
Chandana3 <30951763+Chandana3@users.noreply.github.com>
2020-09-30 13:03:17 +0530

    result of arithmetic calculation

---
+++ result.js
@@ -0,0 +1,11 @@
+const  addition=require('./addition');
+addition.addMethod(200,200);
+
+const subtract=require('./subtract');
+subtract.result(300,200);
+
+const product=require('./product');
+product.multiply(3,2);
+
+const divide=require('./divide');
+divide.quotient(200,200);
