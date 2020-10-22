f0901e9ae5fac5c3de0c1f68202cb1e26a9547f4
Chandana S <sallachandanareddy99@gmail.com>
2020-10-22 16:04:32 +0530

    typescript-examples

---
+++ typescript/SuperMan.ts
@@ -0,0 +1,21 @@
+import {Flyer} from "./Flyer";
+import {Test} from "./Test";
+
+export class SuperMan extends Flyer
+{
+    displayDetails() {
+    }
+
+    fly() {
+    }
+
+    land() {
+    }
+
+    takeOff() {
+
+
+    }
+
+}
+
